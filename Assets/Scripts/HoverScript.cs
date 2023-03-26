785703000e639923fc1801968343d187f4c5055b
SaltyFingerz <denis.iannoukos@yandex.com>
2023-03-26 15:26:23 +0100

    trying floaty hover

---
+++ Assets/Scripts/HoverScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverScript : MonoBehaviour
+{
+    public float amp;
+    public float freq;
+    public float ampHor;
+    public float freqHor;
+ 
+    public float ampDep;
+    public float freqDep;
+    Vector3 initPos;
+    private void Start()
+    {
+        initPos = transform.position;
+
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time * freqHor) * ampHor + initPos.x, Mathf.Sin(Time.time * freq) * amp + initPos.y, Mathf.Sin(Time.time * freqDep) * ampDep + initPos.z);
+
+    }
+   
+}
