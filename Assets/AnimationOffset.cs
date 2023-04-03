cb28c2c3cc6c79310d4b78ca003583e72b123047
SaltyFingerz <denis.iannoukos@yandex.com>
2023-04-03 22:07:18 +0300

    crying loop kinda working, but overly bent, and using patch to not fall through floor

---
+++ Assets/AnimationOffset.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOffset : MonoBehaviour
+{
+    public Animator animBoletus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animBoletus.GetCurrentAnimatorStateInfo(0).IsName("stand-crouchcryloop-f"))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, 1.5f, gameObject.transform.position.z);
+        }
+    }
+}
