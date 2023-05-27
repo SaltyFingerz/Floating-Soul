b24f73c41718a95503b7488e97da57590153f54f
SaltyFingerz <denis.iannoukos@yandex.com>
2023-05-28 00:07:22 +0100

    haptic event on gaining boletus hands, and boletus speech timing fix

--- Assets/OrbHandIOverlapScript.cs
+++ Assets/OrbHandIOverlapScript.cs
@@ -1,11 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class OrbHandIOverlapScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public BoletusInternalMonologue Monologue;
+    InputDevice targetDevice;
+    private XRController xr;
+
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics desiredCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+
+        // Get all devices that match the desired characteristics
+        InputDevices.GetDevicesWithCharacteristics(desiredCharacteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+    }
+
+    void SendHapticEvent(float amplitude, float duration)
+    {
+        // Check if the target device is valid
+        if (targetDevice != null)
+        {
+            // Send the haptic impulse
+            targetDevice.SendHapticImpulse(0, amplitude, duration);
+        }
+    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,9 +42,10 @@ public class OrbHandIOverlapScript : MonoBehaviour
         {
             print("orb hand overlapped");
             Monologue.AAMonologue();
+            SendHapticEvent(1f, 2f);
 
         }
     }
+   
 
-    
 }
